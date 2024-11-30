32acf3aa00fb4451949e26a81b66af2b2298a09f
Luan Scolfaro Amorim Carneiro <scolfaroluan@gmail.com>
2024-11-30 16:16:11 -0300

    botão olho no login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,59 @@
-"use client";
+"use client"; // Diretiva para tornar este um componente de cliente
 
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import API from '../utils/axios';
 import './styles/Login.css';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Ícones para mostrar e ocultar a senha
 
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false); // Estado para controle da visibilidade da senha
+  const router = useRouter();
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            const response = await API.post('/token/', { username, password });
-            localStorage.setItem('access_token', response.data.access);
-            localStorage.setItem('refresh_token', response.data.refresh);
-            API.defaults.headers['Authorization'] = 'Bearer ' + response.data.access;
-            router.push('/home');
-        } catch (error) {
-            console.error('Erro ao fazer login:', error);
-        }
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await API.post('/token/', { username, password });
+      localStorage.setItem('access_token', response.data.access);
+      localStorage.setItem('refresh_token', response.data.refresh);
+      API.defaults.headers['Authorization'] = 'Bearer ' + response.data.access;
+      router.push('/home');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <h1 className='login-title'>LOGIN</h1>
-            <form onSubmit={handleSubmit} className='form-login'>
-                <div className='inputs-labels'>
-                    <h3>Usuário</h3>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        className='inputs'
-                    />
-                </div>
-                <div className='inputs-labels'>
-                    <h3>Senha</h3>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='inputs'
-                    />
-                </div>
-                <button type="submit" className="login-button">Entrar</button>
-            </form>
+  return (
+    <div className="login-container">
+      <h1 className="login-title">LOGIN</h1>
+      <form onSubmit={handleSubmit} className="form-login">
+        <div className="inputs-labels">
+          <h3>Usuário</h3>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="inputs"
+          />
         </div>
-    );
+        <div className="inputs-labels">
+          <h3>Senha</h3>
+          <div className="password-input-container">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="password-input"
+            />
+            <span className="eye-icon" onClick={() => setShowPassword(!showPassword)}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+        </div>
+        <button type="submit" className="login-button">Entrar</button>
+      </form>
+    </div>
+  );
 }
