9e74c48f6a4630e160a63d239e100c64b81c0360
FelipeMaD <felipemarinhodr@hotmail.com>
2024-12-01 14:13:30 -0300

    MUDANÇA DE LAYOUTS

--- src/app/(authenticated)/Home/page.tsx
+++ src/app/(authenticated)/Home/page.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import '../styles/Home.css'
+import '../../styles/Home.css'
 import logo from '../../../public/logo.png'
 // import rightchoice from '../../../public/rightchoice.svg'
 // import leftchoice from '../../../public/leftchoice.svg'
