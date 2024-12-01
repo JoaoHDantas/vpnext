c27b8864713844afdb6fb56225d075095ed64d81
FelipeMaD <felipemarinhodr@hotmail.com>
2024-12-01 17:15:30 -0300

    update crud topAjudantes

--- src/app/(authenticated)/topAjudantes/page.tsx
+++ src/app/(authenticated)/topAjudantes/page.tsx
@@ -4,6 +4,10 @@ import { useState, useEffect } from "react";
 import API from "../../../utils/axios";
 import Link from "next/link";
 import "../../styles/topAjudantes.css";
+import { FaPlus } from "react-icons/fa";
+import { FaHandsHelping } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { AiFillEdit } from "react-icons/ai";
 
 // Define a interface para o tipo TopAjudantes.
 interface TopAjudantes {
@@ -33,12 +37,12 @@ const TopAjudantesList: React.FC = () => {
     }
   };
 
-  
+
   useEffect(() => {
-    
+
     API.get(`/topAjudantes/`)
       .then((response) => {
-        
+
         settopAjudantes(response.data.results);
       })
       .catch((error) => {
@@ -46,52 +50,59 @@ const TopAjudantesList: React.FC = () => {
       });
   }, []);
 
-  
+
   return (
     <div className="top-ajudantes-container">
       {/* Cabeçalho da lista de ajudantes */}
-      <div className="header">
-        <h1>Top Ajudantes</h1>
+      <div className="header-ajudantes">
+        <div id="title">
+          <FaHandsHelping />
+          <h1 id="top-ajud">Top Ajudantes</h1>
+        </div>
+
         <Link href="/topAjudantes/createAjudante" legacyBehavior>
-          <a className="create-button">Adicionar Novo Ajudante</a>
+          <a className="create-button"><FaPlus /></a>
         </Link>
       </div>
       {/* Lista de ajudantes */}
-      <ul>
+      <ul className="listagem-ajudantes">
         {/* Mapeia cada ajudante na lista e renderiza um item de lista para cada um */}
         {topAjudantes.map((topAjudantes) => (
-          <li key={topAjudantes.id} className="ajudante-item">
-            <div className="ajudante-info">
-              {/* Exibe a imagem do perfil, se disponível */}
-              {topAjudantes.profile_picture ? (
-                <img
-                  src={topAjudantes.profile_picture}
-                  alt={topAjudantes.nicknameAjudante}
-                  className="profile-picture"
-                />
-              ) : (
-                <div className="placeholder-picture">Sem Foto</div>
-              )}
-              {/* Exibe o nickname do ajudante */}
-              <span className="ajudante-name">{topAjudantes.nicknameAjudante}</span>
-              {/* Exibe os pontos do post */}
-              <span className="ajudante-points">
-                Pontos: {topAjudantes.postPoints ?? 0}
-              </span>
-            </div>
-            {/* Botões de ação para editar e excluir o ajudante */}
-            <div className="actions">
-              <Link href={`/ajudantes/${topAjudantes.id}/edit`} legacyBehavior>
-                <a className="edit-button">Editar</a>
-              </Link>
-              <button
-                onClick={() => handleDelete(topAjudantes.id)}
-                className="delete-button"
-              >
-                Deletar
-              </button>
-            </div>
-          </li>
+          <Link href={`/topAjudantes/${topAjudantes.id}/`}>
+            <li key={topAjudantes.id} className="ajudante-item">
+              <div className="ajudante-info">
+                {/* Exibe a imagem do perfil, se disponível */}
+                {topAjudantes.profile_picture ? (
+                  <img
+                    src={topAjudantes.profile_picture}
+                    alt={topAjudantes.nicknameAjudante}
+                    className="profile-picture"
+                  />
+                ) : (
+                  <div className="placeholder-picture">Sem Foto</div>
+                )}
+                {/* Exibe o nickname do ajudante */}
+                <span className="ajudante-name">{topAjudantes.nicknameAjudante}</span>
+                {/* Exibe os pontos do post */}
+                <span className="ajudante-points">
+                  Pontos: {topAjudantes.postPoints ?? 0}
+                </span>
+              </div>
+              {/* Botões de ação para editar e excluir o ajudante */}
+              <div className="actions">
+                <Link href={`/topAjudantes/${topAjudantes.id}/edit`} legacyBehavior>
+                  <a className="edit-button"><AiFillEdit /></a>
+                </Link>
+                <button
+                  onClick={() => handleDelete(topAjudantes.id)}
+                  className="delete-button"
+                >
+                  <MdDelete />
+                </button>
+              </div>
+            </li>
+          </Link>
+
         ))}
       </ul>
     </div>
